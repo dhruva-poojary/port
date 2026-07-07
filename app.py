5e616999117a23e34219326a31ac67b6981f8bfb
dhruva-poojary <dhruvapoojary27@gmail.com>
2026-07-07 22:37:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    if __name__ == '__main__':
-    # Adding host='0.0.0.0' opens it to your local Wi-Fi network
     app.run(host='0.0.0.0', port=5000, debug=True)
